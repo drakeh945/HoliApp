d37cfb73d5f454a2b66e981645a59b08e50f4503
drakeh945 <drake.herring@gmail.com>
2022-11-27 10:17:45 -0600

    step 3

---
+++ HoliApp/HoliApp/Cells/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  HoliApp
+//
+//  Created by Drake Herring on 11/27/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
